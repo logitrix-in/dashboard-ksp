9a2bba6ecaf3d9c8ffbbbef29cc9ed90cd6151db
Rownak Mazumder <rmazumder1033@gmail.com>
2024-03-19 23:55:27 +0530

    Done

--- lib/pages/dashboard/body.dart
+++ lib/pages/dashboard/body.dart
@@ -2,63 +2,72 @@ import 'package:dashboard_ksp/pages/dashboard/sections/hero.dart';
 import 'package:dashboard_ksp/pages/dashboard/sections/noOfIncidents.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:resize/resize.dart';
 
 class DashboardBody extends StatelessWidget {
   const DashboardBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                decoration: BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                          color: Colors.grey.withOpacity(0.2), width: 2)),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      width: MediaQuery.of(context).size.width / 4,
-                      color: Colors.white,
-                      child: TextField(
-                        decoration: InputDecoration(
-                            suffixIcon: IconButton(
-                                onPressed: () {}, icon: Icon(Icons.search)),
-                            hintText: "Search",
-                            border: InputBorder.none),
-                      ),
-                    ),
-                    const Gap(30),
-                    Expanded(
-                        child: Container(
-                      alignment: Alignment.centerRight,
-                      color: Colors.white,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.notifications),
-                      ),
-                    ))
-                  ],
-                ))),
+    return Resize(
+      builder: () =>  Container(
         
-        Expanded(
-            flex: 12,
-            child: Container(
-              color: Colors.white,
-              child: Column(
-                children: [
-                  DashboardHero(),
-                  NoIncidents()
-                ],
-              ),
-            ))
-      ],
+        
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                              color: Colors.grey.withOpacity(0.2), width: 2)),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          width: MediaQuery.of(context).size.width / 4,
+                          color: Colors.white,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                    onPressed: () {}, icon: Icon(Icons.search)),
+                                hintText: "Search",
+                                border: InputBorder.none),
+                          ),
+                        ),
+                        const Gap(30),
+                        Expanded(
+                            child: Container(
+                          alignment: Alignment.centerRight,
+                          color: Colors.white,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.notifications),
+                          ),
+                        ))
+                      ],
+                    ))),
+            
+            Expanded(
+                flex: 12,
+                child: Container(
+                  color: Colors.white,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        DashboardHero(),
+                        NoIncidents()
+                      ],
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -1,32 +1,38 @@
 import 'package:dashboard_ksp/pages/dashboard/body.dart';
 import 'package:dashboard_ksp/pages/dashboard/components/navItem.dart';
 import 'package:flutter/material.dart';
+import 'package:resize/resize.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-              child: Container(
-            decoration: const BoxDecoration(
-                color: Color(0xFF1B1919),
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                )),
-            child: const NavItem(),
-          )),
-          Expanded(
-              flex: 5,
-              child: Container(
-                color: const Color(0xFFFEFCF6),
-                child: DashboardBody(),
-              ))
-        ],
+    return Resize(
+      builder: () =>  Scaffold(
+        body: Row(
+          children: [
+            Expanded(
+                child: Container(
+              decoration: const BoxDecoration(
+                  color: Color(0xFF1B1919),
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  )),
+              child: const NavItem(),
+            )),
+            Expanded(
+                flex: 5,
+                child: SingleChildScrollView(
+                  child: Container(
+                    height: 100.vh,
+                    color: const Color(0xFFFEFCF6),
+                    child: DashboardBody(),
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
